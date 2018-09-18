341775290aac71f6518b781c81d6ebb29e9b77cd
Sangwook Nam <nso001@naver.com>
2018-09-19 00:44:47 +0900

    Init test project

---
+++ CarthageTest/CarthageTest.h
@@ -0,0 +1,19 @@
+//
+//  CarthageTest.h
+//  CarthageTest
+//
+//  Created by 남상욱 on 2018. 9. 19..
+//  Copyright © 2018년 남상욱. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CarthageTest.
+FOUNDATION_EXPORT double CarthageTestVersionNumber;
+
+//! Project version string for CarthageTest.
+FOUNDATION_EXPORT const unsigned char CarthageTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CarthageTest/PublicHeader.h>
+
+
---
+++ CarthageTest/CustomTextView.swift
@@ -0,0 +1,21 @@
+//
+//  CustomTextView.swift
+//  CarthageTest
+//
+//  Created by 남상욱 on 2018. 9. 19..
+//  Copyright © 2018년 남상욱. All rights reserved.
+//
+
+import UIKit
+
+class CustomTextView: UITextView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
